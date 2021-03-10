5b559b81c80e3a1b0effadeff2eef85cb426492a
Alex Collin <a.i.kollin.viscur@gmail.com>
2021-03-10 08:44:46 +0300

    Add admin

---
+++ admin/app/admin/authors.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Author do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :deleted_at, :name, :is_blocked
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:deleted_at, :name, :is_blocked]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
---
+++ admin/app/admin/posts.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Post do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :deleted_at, :author, :type, :pair, :date, :image, :descr, :url, :title
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:deleted_at, :author, :type, :pair, :date, :image, :descr, :url, :title]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
